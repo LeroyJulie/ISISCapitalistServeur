af6e76d5e2c1468d1e25ee90de3384881a9023fc
JulieLeroy <julie.leroy@etud.univ-jfc.fr>
2022-03-05 11:28:50 +0100

    maj

--- src/main/java/com/example/ISISCapitalist2/Services.java
+++ src/main/java/com/example/ISISCapitalist2/Services.java
@@ -6,6 +6,16 @@
 package com.example.ISISCapitalist2;
 
 import generated.World;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.UnmarshalException;
+import javax.xml.bind.Unmarshaller;
 
 /**
  *
@@ -13,19 +23,31 @@ import generated.World;
  */
 public class Services {
     
-    private World readWorldFromXml();
-         InputStream input = getClass().getClassLoader().getRessourceAsStream("ressources\world.xml");
-                 
-                 
-                 
-                 
-    private void saveWordlToXml(World world);
-          OutoutStream output = new FileOutputStream(file);  
+    private World readWorldFromXml() throws JAXBException{
+        JAXBContext cont = JAXBContext.newInstance(World.class);
+        Unmarshaller m = cont.createUnmarshaller();
+        World world;
+        try {
+            world = (World) m.unmarshal(new File("world.xml"));
+        } catch (UnmarshalException e) {
+            InputStream input = getClass().getClassLoader().getResourceAsStream("world.xml");
+            world = (World) m.unmarshal(input);
+        }
+        return world;
+    }
+         
+                                 
+    private void saveWordlToXml(String username, World world) throws FileNotFoundException, JAXBException {
+        JAXBContext cont = JAXBContext.newInstance(World.class);
+        Marshaller m = cont.createMarshaller();
+        OutputStream os = new FileOutputStream("world.xml");
+        m.marshal(world, os);
+    }
     
     
     
-    
-    public World getWorld();
+    public World getWorld() throws JAXBException{
         World world = readWorldFromXml();
         return world;
+    }
 }
--- src/main/java/com/example/ISISCapitalist2/Webservice.java
+++ src/main/java/com/example/ISISCapitalist2/Webservice.java
@@ -5,10 +5,35 @@
  */
 package com.example.ISISCapitalist2;
 
+import generated.World;
+import javax.xml.bind.JAXBException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Utilisateur
  */
-public class Webservice {
-    
-}
+
+
+    @RestController
+    @RequestMapping("adventureisis/generic")
+    @CrossOrigin
+    public class Webservice {
+
+        Services services;
+
+        public Webservice() {
+            services = new Services();
+        }
+
+        @GetMapping(value = "world", produces = {"application/xml", "application/json"})
+
+        public ResponseEntity<World> getWorld() throws JAXBException {
+            World world = services.getWorld();
+            return ResponseEntity.ok(world);
+        }
+    }
